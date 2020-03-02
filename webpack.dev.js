d31fe17d37e3a442c6d4caf0ec5eeca7c8215061
arefeenm <arefeen@newscred.com>
2020-03-02 12:31:51 +0600

    Add webpack configs

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+const common = require('./webpack.common.js'),
+  merge = require('webpack-merge'),
+  path = require('path'),
+  webpack = require('webpack');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.BASE_URL': JSON.stringify('http://localhost:3000'),
+      'process.env.API_BASE_URL': JSON.stringify('http://localhost:8080/api')
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'static'),
+    watchContentBase: true,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  }
+});
---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+const common = require('./webpack.common.js'),
+  merge = require('webpack-merge'),
+  path = require('path'),
+  webpack = require('webpack');
+
+const API_BASE_URL = '<YOUR_API_SERVER>';
+const BASE_URL = '<YOUR_FRONTEND_CLIENT>';
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.BASE_URL': JSON.stringify(BASE_URL),
+      'process.env.API_BASE_URL': JSON.stringify(API_BASE_URL)
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  devServer: {
+    allowedHosts: [
+      '*'
+    ],
+    public: '*',
+    port: 3000,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    contentBase: path.join(__dirname, 'static'),
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  }
+});
